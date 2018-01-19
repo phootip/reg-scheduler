93208057ebd01b727c360bdcb4f68e015881fe4b
peawyoyoyin <peawyoyoyin@gmail.com>
2018-01-19 20:27:29 +0700

    disable eslint's 'linebreak-style' rule. 'linebreak-style' rule generates excess compile problems for windows which use CRLF, however, allowing CRLF will not lead to inconsistent linebreak styles because git automatically converts CRLF to LF when committing changes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
       js: 'never',
       vue: 'never'
     }],
+    'linebreak-style': 'off',
     // disallow reassignment of function parameters
     // disallow parameter object manipulation except for specific exclusions
     'no-param-reassign': ['error', {
