f7e2ae686093bb1fc092005d3720ad3ac06f8697
blead <blead010@gmail.com>
2018-01-29 13:28:20 +0700

    Fix merge errors and clean up

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,7 @@ import Buefy from 'buefy';
 import 'buefy/lib/buefy.css';
 import Main from '@/components/Main';
 import About from '@/components/About';
-import HelloWorld from '@/components/HelloWorld';
 import ScheduleManager from '@/components/ScheduleManager/ScheduleManager';
-import ScheduleTable from '@/components/ScheduleTable';
 
 Vue.use(Router);
 Vue.use(Buefy);
