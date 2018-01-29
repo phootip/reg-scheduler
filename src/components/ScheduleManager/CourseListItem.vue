a92c2b0582a927549e75352b30c9d59ff4fc0f1a
peawyoyoyin <peawyoyoyin@gmail.com>
2018-01-29 19:48:07 +0700

    SideBar now show a list of all courses in the app

---
+++ src/components/ScheduleManager/CourseListItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="media-content">
+        <span class="title is-size-4">
+          {{ course.name || 'No Name'}}
+        </span>
+        <span class="subtitle is-size-6">
+          {{ course.code || ''}}
+        </span>
+      </div>
+      <br/>
+      <table class="table is-narrow is-marginless">
+        <thead>
+          <th>#</th>
+          <th>Teacher</th>
+          <th class="has-text-centered">Time</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ selectedSection.number || ''}}</td>
+            <td>{{ selectedSection.teacher }}</td>
+            <td>
+              <table class="table is-narrow" style="margin: 0;">
+                <tbody style="margin: 0;">
+                  <tr style="margin-top: 0;" v-for="(timeRange, index) in selectedSection.timeRanges" :key="index">
+                    <td class="is-uppercase" style="padding-top: 0;">{{timeRange.day}}</td>
+                    <td style="padding-top: 0;">{{timeRange.start}}</td>
+                    -
+                    <td style="padding-top: 0;">{{timeRange.end}}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="card-footer">
+      <a href="#" class="card-footer-item">Change</a>
+      <a href="#" class="card-footer-item">Unselect</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScheduleManagerCourseListItem',
+  props: {
+    course: {
+      type: Object,
+      default() {
+        return {
+          name: 'ERROR',
+          code: 'ERROR',
+        };
+      },
+    },
+    selectedSectionNumber: {
+      type: Number,
+      default() {
+        return 0;
+      },
+    },
+  },
+  computed: {
+    selectedSection() {
+      return this.course.sections.filter(section => section.number === this.selectedSectionNumber)[0];
+    },
+  }
+}
+</script>
--- src/components/ScheduleManager/SideBar.vue
+++ src/components/ScheduleManager/SideBar.vue
@@ -7,38 +7,78 @@
     </div>
     <div class="hero">
       <div class="hero-body">
-        <p>
-          <!-- eslint-disable max-len -->
-          <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id commodi optio aspernatur aliquam explicabo quidem praesentium veritatis, iste voluptas vero. Officia unde ipsa eligendi et, optio aut voluptate minus aliquam?</span>
-          <span>Qui voluptate perspiciatis quae accusamus animi, sint atque quaerat illum est alias ducimus dolores blanditiis ipsum debitis tenetur? Illo laboriosam, quam qui commodi non debitis tempore sunt accusantium doloremque dolor.</span>
-          <span>Cum ducimus adipisci, doloribus dignissimos pariatur delectus saepe ipsam totam? Nemo, maiores! Tempora consectetur harum labore dignissimos, qui mollitia vero iure omnis nihil cum accusamus nisi beatae rerum voluptas ea?</span>
-          <span>Unde, atque. Ullam, quae unde libero temporibus dicta architecto delectus dolorem nesciunt vero asperiores distinctio praesentium eveniet tempora molestias ipsam est laboriosam nostrum voluptatum quisquam! Quam corporis facilis nobis porro.</span>
-          <span>Praesentium ex, sapiente debitis ipsum voluptatem omnis, beatae atque assumenda vero laborum id neque et reprehenderit. Sit voluptas, minima, maiores reprehenderit iste illo quam rerum ab molestiae aspernatur officiis doloremque?</span>
-          <span>Iusto, explicabo qui dignissimos veritatis ex a alias nemo. Ea, molestias labore impedit reprehenderit cupiditate omnis dolor possimus libero, aspernatur fuga nam dolorum ab suscipit tempora consectetur ipsum praesentium! Officia!</span>
-          <span>Voluptates aperiam ea quisquam laboriosam adipisci quibusdam. Aut provident animi vitae, beatae suscipit ipsum ea vel tempora quia voluptas quod, blanditiis adipisci veniam harum. Dolores eos ex possimus quaerat reiciendis.</span>
-          <span>Eveniet rerum omnis natus laboriosam cum saepe quibusdam voluptatum, delectus perferendis atque tempora dolorem iste eligendi reiciendis optio maxime aperiam recusandae cumque magni soluta. Quo debitis sed perspiciatis laudantium dolorum?</span>
-          <span>Qui nesciunt recusandae eveniet rerum optio praesentium voluptas vero deleniti minima nihil impedit dolorem quaerat quidem voluptatum, aspernatur, corporis, neque exercitationem natus earum distinctio voluptatem. Eveniet explicabo illo quidem fugit?</span>
-          <span>Maxime, incidunt. Ex perspiciatis laborum quisquam aperiam, maiores adipisci sapiente similique iste excepturi explicabo optio voluptates nemo, vitae, tempora deleniti iure ab ipsam. Expedita cumque aliquam repellendus dolorum iste beatae.</span>
-          <span>Ipsa, ex. At, laboriosam! Laborum quas laudantium ab velit fugiat illum, earum maiores illo ipsam nemo necessitatibus dolor quisquam! Omnis distinctio nam labore eius molestias quis animi esse dignissimos beatae.</span>
-          <span>Quasi rem itaque esse nisi sint necessitatibus, error modi ex odio quisquam. Accusamus vitae eius eveniet aliquam aut dolor officia eos illo et in molestiae similique blanditiis unde, ratione hic?</span>
-          <span>Deleniti numquam quod quis harum? Cum eligendi ipsum ut similique soluta fugiat eum quae optio ipsa! Totam, a. Quaerat, commodi. Temporibus nisi voluptates iste impedit neque aliquid, mollitia dolores! Deserunt!</span>
-          <span>Omnis consequuntur esse impedit commodi repellendus adipisci hic numquam exercitationem tempora voluptatum quas nesciunt fuga totam nisi consequatur, facilis vel doloremque explicabo velit? Excepturi optio ducimus maxime eveniet, quam corrupti?</span>
-          <span>Facere dolorum fugit non, animi placeat officia eum mollitia commodi eveniet ratione recusandae, rem quo? Nesciunt, facere officiis repellat perferendis quo maiores exercitationem eveniet, aperiam culpa dolor unde corporis cum.</span>
-          <span>Iure corrupti ex ipsa eius officia sit voluptate labore. Et tempora repellendus debitis ab aspernatur provident pariatur a dignissimos iusto ipsam repudiandae, eos maiores quia tempore voluptatibus dicta dolore quas.</span>
-          <span>Ad atque aliquam quidem pariatur facilis harum ipsam laborum dolore a earum neque libero itaque non adipisci, suscipit corrupti id laudantium tempore placeat, est veritatis voluptatem vel cum quaerat. Vitae.</span>
-          <span>Fuga, tempora modi deleniti velit numquam nemo dolores? Blanditiis, nobis vel. Sequi consequatur est ullam non tempore perferendis consectetur vel sed quam. Porro quam, vitae deserunt quas necessitatibus consequatur exercitationem.</span>
-          <span>Vel consectetur nulla, recusandae magnam nihil totam accusantium quod tenetur labore, rem omnis in inventore cum assumenda neque optio numquam quisquam! Expedita delectus vitae a, commodi non ratione saepe alias!</span>
-          <span>Facilis consequuntur aut amet ut explicabo quam fugiat quo ducimus nemo possimus? Accusamus aspernatur fugiat iste reprehenderit velit facilis voluptate, veniam maxime. Sunt quos tempora fuga consectetur aliquam tempore sit.</span>
-          <!-- eslint-enable max-len -->
-        </p>
+        <ul id="scheduleManagerCourseList">
+          <li v-for="course in courses" :key="course.name">
+            <CourseListItem :course="course" :selectedSectionNumber="1" />
+            <br/>
+          </li>
+        </ul>
       </div>
     </div>
+    <div class="hero">
+        <a href="#" class="button is-success">
+          Add another Course
+        </a>
+    </div>
+    <br/>
   </div>
 </template>
 
 <script>
+import ScheduleManagerCourseListItem from './CourseListItem'
 export default {
   name: 'ScheduleManagerSideBar',
+  components: {
+    CourseListItem: ScheduleManagerCourseListItem,
+  },
+  data() {
+    return {
+      courses: [
+        {
+          name: 'Prog Lang',
+          code: '2110316',
+          sections: [
+            {
+              number: 1,
+              teacher: 'ASS',
+              timeRanges: [
+                { day: 'mon', start: '18:00', end: '19:00' },
+              ]
+            },
+            {
+              number: 2,
+              teacher: 'DAM',
+              timeRanges: [
+                { day: 'mon', start: '18:00', end: '19:00' },
+                { day: 'tue', start: '18:00', end: '19:00' }
+              ]
+            }
+          ],
+        },
+        {
+          name: 'Comp Eng Ess',
+          code: '2110221',
+          sections: [
+            {
+              number: 1,
+              teacher: 'AST',
+              timeRanges: [
+                { day: 'mon', start: '18:00', end: '19:00' },
+                { day: 'tue', start: '18:00', end: '19:00' }
+              ]
+            },
+            {
+              number: 2,
+              teacher: 'DAM',
+              timeRanges: [
+                { day: 'mon', start: '18:00', end: '19:00' },
+              ]
+            }
+          ],
+        },
+      ]
+    };
+  },
 };
 </script>
 
